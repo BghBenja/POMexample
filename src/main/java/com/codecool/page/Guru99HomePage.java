bf4b7f500dbcbe8e9f3aed825b8d7628b02385e1
BghBenja <bghbenja@gmail.com>
2023-01-12 17:53:43 +0100

    Add fields and constructor to Guru99HomePage

--- src/main/java/com/codecool/page/Guru99HomePage.java
+++ src/main/java/com/codecool/page/Guru99HomePage.java
@@ -1,4 +1,19 @@
 package com.codecool.page;
 
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+
 public class Guru99HomePage {
+
+    WebDriver driver;
+
+    By homePageUserName = By.xpath("//table//tr[@class='heading3']");
+
+
+    public Guru99HomePage(WebDriver driver) {
+
+        this.driver = driver;
+
+    }
 }
