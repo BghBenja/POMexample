50beb9ac3cb2d9db60c73312b7e89e25641da05c
BghBenja <bghbenja@gmail.com>
2023-01-12 17:41:56 +0100

    Add fields and constructor to Guru99Login

--- src/main/java/com/codecool/page/Guru99Login.java
+++ src/main/java/com/codecool/page/Guru99Login.java
@@ -1,4 +1,24 @@
 package com.codecool.page;
 
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+
 public class Guru99Login {
+
+    WebDriver driver;
+
+    By user99GuruName = By.name("uid");
+
+    By password99Guru = By.name("password");
+
+    By titleText = By.className("barone");
+
+    By login = By.name("btnLogin");
+
+    public Guru99Login(WebDriver driver) {
+
+        this.driver = driver;
+
+    }
 }
