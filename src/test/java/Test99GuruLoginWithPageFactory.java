5fa97e45c0c9ddf33f6bde73c2cf1b5a5fc34a2e
BghBenja <bghbenja@gmail.com>
2023-01-17 16:44:23 +0100

    Add fields to Test99GuruLoginWhithPageFactory class

---
+++ src/test/java/Test99GuruLoginWithPageFactory.java
@@ -0,0 +1,14 @@
+import com.codecool.page.Guru99HomePage;
+import com.codecool.pageFactory.Guru99Login;
+import org.openqa.selenium.WebDriver;
+
+public class Test99GuruLoginWithPageFactory {
+
+    String driverPath = "";
+
+    WebDriver driver;
+
+    Guru99Login objLogin;
+
+    Guru99HomePage objHomePage;
+}
