68c77f8f4ee0f3feb43a7dee03aad42d4ba3d56f
BghBenja <bghbenja@gmail.com>
2023-01-17 16:47:09 +0100

    Implement setup() to Test99GuruLoginWhithPageFactory class

--- src/test/java/Test99GuruLoginWithPageFactory.java
+++ src/test/java/Test99GuruLoginWithPageFactory.java
@@ -1,6 +1,10 @@
 import com.codecool.page.Guru99HomePage;
 import com.codecool.pageFactory.Guru99Login;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
 
 public class Test99GuruLoginWithPageFactory {
 
@@ -11,4 +15,18 @@ public class Test99GuruLoginWithPageFactory {
     Guru99Login objLogin;
 
     Guru99HomePage objHomePage;
+
+    @BeforeTest
+
+    public void setup(){
+
+        System.setProperty("webdriver.gecko.driver", driverPath);
+
+        driver = new FirefoxDriver();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("http://guru99.com/V4/");
+
+    }
 }
