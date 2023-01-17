fe606d38c62455b717f94363f67ebc12ac3122e5
BghBenja <bghbenja@gmail.com>
2023-01-17 16:36:05 +0100

    Changes Guru99HomePage with PageFactory

--- src/main/java/com/codecool/page/Guru99HomePage.java
+++ src/main/java/com/codecool/page/Guru99HomePage.java
@@ -1,25 +1,28 @@
 package com.codecool.page;
-
-import org.openqa.selenium.By;
-
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Guru99HomePage {
 
     WebDriver driver;
 
-    By homePageUserName = By.xpath("//table//tr[@class='heading3']");
+    @FindBy(xpath = "//table//tr[@class='heading3']")
 
+    WebElement homePageUserName;
 
     public Guru99HomePage(WebDriver driver) {
 
         this.driver = driver;
 
+        PageFactory.initElements(driver, this);
+
     }
 
     public String getHomePageDashboardUserName() {
 
-        return driver.findElement(homePageUserName).getText();
+        return homePageUserName.getText();
 
     }
 }
